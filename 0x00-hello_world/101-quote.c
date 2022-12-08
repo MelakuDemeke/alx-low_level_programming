238bf6d95fc0239ba38643276b838d4508816a9e
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-08 05:45:06 -0800

    qote added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - starting point of programm
+ *
+ * Description; outputs a text
+ *
+ * Return: 0 -> to indicate end
+ *
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
