da6c4a46ddffc5fbcfb30ea99c9914ff3e7e4257
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-08 04:34:30 -0800

    first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ *
+ * Description: prints out a text
+ *
+ * Return: 0 to indicate end of execution
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+	return (0);
+}
