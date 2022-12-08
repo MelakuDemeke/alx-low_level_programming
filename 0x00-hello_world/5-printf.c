31440aa23ad5cf2e4677656a4edcbbb655cf684f
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-08 04:54:52 -0800

    printf code added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ *
+ * Description: printf a sentece
+ *
+ * Return: 0 indicating end of programm
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
