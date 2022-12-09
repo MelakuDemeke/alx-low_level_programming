09c16b02b3b96231e1d2067fff040d4179e91b96
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-09 16:36:15 +0300

    number in put char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - prints numbers
+ *
+ * Description: prints out 0-9
+ *
+ * Return: 0-> indicate end
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(48 + i)
+	}
+	putchar('\n');
+	return (0);
+}
