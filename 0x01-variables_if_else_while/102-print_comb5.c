f9d494314df94b1ba053a9918dcb07a199d77ce3
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-09 17:37:51 +0300

    combination of 3 digit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ *
+ * Description: print all possible different combinations of three digits.
+ *
+ * Return: 0-> indicate end
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(' ');
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			if (j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
