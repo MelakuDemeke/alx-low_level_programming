3c8540a559f882972d98a6a4d6716caead5c2055
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-09 16:14:29 +0300

    alpha exept e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabets
+ *
+ * Description: prints out a-z exept q & e
+ *
+ * Return: 0-> indicate end
+ *
+ */
+int main(void)
+{
+	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+			'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+			'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (!(alpha[i] == 'q' || alpha[i] == 'e'))
+		{
+			putchar(alpha[i]);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
