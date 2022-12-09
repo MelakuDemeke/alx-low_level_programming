c325a819005050422970b1ff517d85623f8037a1
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-09 16:42:47 +0300

    alpabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabets
+ *
+ * Description: prints out z-a in revers
+ *
+ * Return: 0-> indicate end
+ *
+ */
+int main(void)
+{
+	char start;
+	char end = 'a';
+
+	for (start = 'z'; start >= end; start--)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
