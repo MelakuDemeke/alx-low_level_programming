7d2da203dde6bd8f86900a578851372c254d4b50
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-09 16:59:02 +0300

    digit with separetor

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - prints all combined numbers
+ *
+ * Description: all possible combinations of single-digit numbers
+ *
+ * Return: 0-> indicate end
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 9)
+		{
+			putchar(48 + i);
+		}
+		else
+		{
+		putchar(48 + i);
+		putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
