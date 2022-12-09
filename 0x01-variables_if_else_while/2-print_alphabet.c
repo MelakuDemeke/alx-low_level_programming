31ad0a84945d6a9a3fe2920969086ff9982d9621
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-09 04:34:16 -0800

    declartion eroor fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,7 +13,9 @@ int main(void)
 	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 			'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
 			'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	for (int i = 0; i < 26; i++)
+	int i;
+
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alpha[i]);
 	}
