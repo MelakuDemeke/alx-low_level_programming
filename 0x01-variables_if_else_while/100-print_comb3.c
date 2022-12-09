8407a8519cf8cf3f49bbf43f688a7811c9df6a04
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-09 17:15:47 +0300

    combo digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - prints all combined 2digit numbers
+ *
+ * Description: print all possible different combinations of two digits
+ *
+ * Return: 0-> indicate end
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(48 + i);
+			putchar(48 + j);
+			if (i != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	    }
+	}
+	putchar('\n');
+	return (0);
+}
