153649b865b39dfc0f53b15c2d17b776f57257dd
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 13:48:49 +0300

    print alpha 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet a - z 10 times
+ *
+ * Description: when the function get called it prints a - z 10 times
+ *		using the _putchar function and put new line
+ *		at the end
+ * Return: 0-> shows end void no return
+ *
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+	
+}
