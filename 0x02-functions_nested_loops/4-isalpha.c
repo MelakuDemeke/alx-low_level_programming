0cf550291c60fa1898a737d6b386f17ff62e079a
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 15:02:48 +0300

    final chek

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,11 +12,7 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (0);
-	}
-	else if(c >= 65 && c >= 90)
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
