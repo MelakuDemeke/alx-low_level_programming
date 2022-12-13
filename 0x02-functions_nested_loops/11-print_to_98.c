b3a2cd85401d4eb53c2039f51e3a0d101334862e
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 16:50:32 +0300

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_to_98 -  prints natural numbers from n to 98,
+ *
+ * Description: when the function get called it prints numbers to 98
+ *
+ * @n: starting number
+ *
+ * Return: void
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if ( n == 0)
+	{
+		_putchar(n);
+	}
+	else if ( n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n')
+}
