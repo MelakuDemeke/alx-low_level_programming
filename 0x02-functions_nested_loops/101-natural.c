705944bfd50abfc8dbcbff1c88b3ee312d5aeaaa
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 20:20:00 +0300

    sum of natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * add - add
+ *
+ * Description: print sum of all multiple of 3 or 5 below 1024
+ * 
+ * Return: the value of the sum
+ *
+ */
+
+int main(void)
+{
+	int sum, i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+		
+	}
+}
