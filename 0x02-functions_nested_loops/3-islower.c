e40ab1e9ac9c0fcfc9a30f32a9a6b2b4ce495a22
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 14:31:47 +0300

    alpha cheker

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _islower - prints
+ * _islower - prints 1 or 0
  *
- * Description: when the function get called it check if lower
- *		anf if it is return 1 if not 0
- * @c: the number tobe cheked
+ * Description: when the function get called it check if alpha
+ *		and if it is return 1 if not 0
+ * @c: the alphabet tobe cheked
  * Return: 0-> shows end void no return
  *
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints
+ *
+ * Description: when the function get called it check if lower
+ *		anf if it is return 1 if not 0
+ * @c: the number tobe cheked
+ * Return: 0-> shows end void no return
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
