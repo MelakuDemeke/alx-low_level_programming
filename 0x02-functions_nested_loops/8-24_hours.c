438bd21ea3a39d6ad386dddfda6f946601dc846d
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 15:59:35 +0300

    fix to betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,17 +4,19 @@
  * jack_bauer - prints every minute of the day of Jack Bauer
  *
  * Description: when the function get called it prints 00:00 - 23:59
- * 
+ *
  * Return: void
  *
  */
 
 void jack_bauer(void)
 {
-	int i,j;
+	int i, j;
 
-	for (i = 0; i < 24; i++){
-		for (j = 0; j < 60; j++){
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
