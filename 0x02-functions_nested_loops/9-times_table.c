9ee13b4736b5d406514502efd07fc0db87ca4c5e
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 16:20:16 +0300

    minor fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,13 +11,14 @@
 
 void times_table(void)
 {
-	int i, j; mul;
+	int i, j; 
+	int mul;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			mul = i*j;
+			mul = (i * j);
 			_putchar(mul);
 			_putchar(', ')
 		}
