32d108dd19b0f13f6cea5beb3681dc7222cabd25
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 14:22:14 +0300

    parameter description added

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,13 +5,14 @@
  *
  * Description: when the function get called it check if lower
  *		anf if it is return 1 if not 0
+ * @c: the number tobe cheked
  * Return: 0-> shows end void no return
  *
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
