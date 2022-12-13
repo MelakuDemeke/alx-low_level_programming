318b0b150563c4869645a8d0c50ca2dc40ed4e64
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 14:43:43 +0300

    all fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _islower - prints 1 or 0
+ * _islower - prints
  *
- * Description: when the function get called it check if alpha
- *		and if it is return 1 if not 0
- * @c: the alphabet tobe cheked
+ * Description: when the function get called it check if lower
+ *		anf if it is return 1 if not 0
+ * @c: the number tobe cheked
  * Return: 0-> shows end void no return
  *
  */
 
 int _islower(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _isalpha - prints
+ * _islower - prints 1 or 0
  *
- * Description: when the function get called it check if lower
- *		anf if it is return 1 if not 0
- * @c: the number tobe cheked
+ * Description: when the function get called it check if alpha
+ *		and if it is return 1 if not 0
+ * @c: the alphabe tobe cheked
  * Return: 0-> shows end void no return
  *
  */
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c >=90))
 	{
 		return (1);
 	}
