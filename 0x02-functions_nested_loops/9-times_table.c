f9984befe4d4ff47e84ea40965c583b1c3fb6fc2
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 16:18:57 +0300

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Description: when the function get called it prints time table up to 9
+ *
+ * Return: void
+ *
+ */
+
+void times_table(void)
+{
+	int i, j; mul;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			mul = i*j;
+			_putchar(mul);
+			_putchar(', ')
+		}
+		_putchar('\n')
+	}
+}
