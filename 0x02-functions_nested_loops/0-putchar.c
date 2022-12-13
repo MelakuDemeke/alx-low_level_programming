4939d8846df4168e4d9c6308a61123324f9ba56f
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 13:10:44 +0300

    code fix to betty standard

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
 #include "main.h"
+
 /**
  * main - starting of the program
- * 
- * Description: prints _putchar 
- * 
+ *
+ * Description: prints _putchar
+ *
  * Return: 0-> endicated end
- * 
+ *
 */
 
 int main(void)
 {
     char name[] = "_putchar";
-    int i
+    int i;
 
     for (i = 0; i < 8; i++){
         _putchar(name[i]);
