c97393044c82838424001cdccaa134488a497350
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-13 15:57:54 +0300

    24 hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Description: when the function get called it prints 00:00 - 23:59
+ * 
+ * Return: void
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i,j;
+
+	for (i = 0; i < 24; i++){
+		for (j = 0; j < 60; j++){
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
