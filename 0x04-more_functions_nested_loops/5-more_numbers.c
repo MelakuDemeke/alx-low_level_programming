648cf3767355d22ec84ed2d945396642cc97e4ba
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-15 14:42:24 +0300

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* more_numbers - print 0 - 14 10 times
+*
+* Return: 0-> end of program
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15;j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+			_putchar('\n');
+	}
+}
