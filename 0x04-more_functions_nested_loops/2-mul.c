35a2672783398e6c037188cfa454b032d81e19ea
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-15 13:29:18 +0300

    mul added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiplay two numbers
+* @a: operand one
+* @b: operand two
+* Return: 1 -> if digit , 0 -> if not
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
