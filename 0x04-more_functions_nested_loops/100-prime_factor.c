1292d9ecbfca47d1d85473808610451285023269
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-15 20:31:11 +0300

    prime factorial finder

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+* main - find and print prime factor of 612852475143
+*
+*
+* Return: 0-> end of program
+*/
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int divsor = 2, result = 0, maxFact;
+
+	while(n!=0)
+	{
+		if(n % divsor !=0)
+			divsor = divsor + 1;
+		else 
+		{
+			maxFact = n;
+			n = n / divsor;
+			if(n == 1)
+			{
+				printf("%d\n",maxFact);
+				result = 1;
+				break;
+			}
+		}
+	}
+	return 0;
+}
