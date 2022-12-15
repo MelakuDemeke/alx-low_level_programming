cef01ab417ae119969b1422895738012d56173f0
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-15 16:58:13 +0300

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* main - starting point
+*
+*
+* Return: 0-> end of program
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
