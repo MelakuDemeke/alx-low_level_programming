c3a3ba8f56ab29917c09427ece04247814c1063f
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-15 13:52:41 +0300

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - print 0 - 9
+*
+* Return: 0-> end of program
+*/
+
+print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
