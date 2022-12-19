815152db1fec5034167f6eb88c4abec7e1672f6d
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-19 14:27:38 +0300

    put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* puts_half - print half of string
+*
+* @str: the string to be cut in half
+*/
+
+void puts_half(char *str)
+{
+	int i, half;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	half = i / 2;
+	for (i = half + 1; str[i] != 0; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
