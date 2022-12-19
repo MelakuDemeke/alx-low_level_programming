ef4c9ce771194a09bd66e9a24690442bff0e5330
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-19 13:01:07 +0300

    swap added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* swap_int - swap the value of two int
+*
+* @a: value 1 to be swaped
+* @b: value 2 to be swaped
+*/
+void swap_int(int *a, int *b)
+{
+	int temp;
+	tmp = *a;
+	*a = *b;
+	*b = temp;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 #include<stdio.h>
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
