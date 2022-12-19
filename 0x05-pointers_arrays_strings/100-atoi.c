cfd8728d94569e95e434dd25a482dfb0e227b09f
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-19 15:10:10 +0300

    100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* _atoi - convert string to int
+*
+* @s: string tobe converted
+*/
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do
+	{
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = (num * 10) + (*s - '0');
+		}
+		else if (num > 0){
+			break;
+		}
+	}
+	while (*s++);
+	return (num * sign);
+}
