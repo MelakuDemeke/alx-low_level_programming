085501f6e1fa79db1faaa22e81a0ec57c4f85e7e
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-19 13:15:00 +0300

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen - prints a length of string
+*
+*@s: string to be counted
+*/
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while(s[counter] != '\0')
+	{
+		counter++;
+	}
+
+	return (counter);
+}
