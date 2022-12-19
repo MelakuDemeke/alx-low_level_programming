86e5de0375cb538709873b3aca5bc172ae248058
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-19 14:45:17 +0300

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_array - print n elements of array
+*
+* @a: array
+* @n: index of array
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
