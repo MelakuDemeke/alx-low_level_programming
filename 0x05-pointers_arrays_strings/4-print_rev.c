432ee415b4efbb23f6e263ee2c9c589e3ab90cf8
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-19 13:43:55 +0300

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_rev - prints string in revers
+*
+* @s: the string to be reversed
+*/
+
+void print_rev(char *s)
+{
+	int counter = 0;
+
+	while (s[counter] != 0)
+	{
+		counter++
+	}
+	while (counter > 0)
+	{
+		counter--;
+		_putchar(s[counter]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _putchar();
 void _puts(char *str);
+void print_rev(char *s);
