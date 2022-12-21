642ce42b7542ecbb5af58d38c97b32cab2496714
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-21 12:00:17 +0300

    strcpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strncpy - copy one string to other
+*
+* @dest: destination string
+* @src: starting string
+* @n: limiter
+*
+* Return: result of dest
+*/
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; j < n; j++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
