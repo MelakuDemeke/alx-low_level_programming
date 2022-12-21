89106ee9d3981a0ea49ea78bdd2eea6f3847f4cc
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-21 14:27:32 +0300

    cpat string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* cap_string - reverse array
+*
+* @s: string to change to word upper
+*
+* Return: upper string
+*/
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == ';' || s[i] == '\n' || s[i] == '\t'
+		|| s[i] == '{' || s[i] == '}' || s[i] == '(' || s[i] == ')'
+		|| s[i] == ',' || s[i] == '.' || s[i] == '!' || s[i] == '?'
+		|| s[i] == '"')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+			i++;
+		}
+	}
+	if (s[0] >= 'a' && s[i] <= 'z')
+	{
+		s[0] = s[0] - 32; 
+	}
+	return (s);
+}
