1a388476242150522ccf771a3f22fac27bc7267d
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-21 11:35:22 +0300

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strncat - concatinates two string
+*
+* @dest: destination string
+* @src: starting string
+*
+* Return: result of dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
