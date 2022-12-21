3eccfde17d2b3619b3d3f3d34c828592d19b4a5d
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-21 15:40:50 +0300

    rot code added

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+* rot13 - encodes a sing to rot13
+*
+* @s: sing to be encoded
+*
+* Return: encoded sing
+*/
+
+char *rot13(char *s)
+{
+	int j = 0, i;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (s[j])
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (s[j] == alphabet[i])
+			{
+				s[j] = rot13key[i];
+				break;
+			}
+		}
+
+		j++;
+
+	}
+	return (s);
+}
