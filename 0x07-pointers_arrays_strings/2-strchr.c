350ea962e5065308b8bb0746416705a525becc0c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-01-02 11:11:10 +0300

    strchar

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strchr - locate character instring
+*
+* @s: destination buffer
+* @c: source to copy
+*
+* Return: occurence or null if none
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s != '\0')
+		{
+			return(s);
+		}
+		s++;
+	}
+	if (*S == c)
+	{
+		return(s);
+	}
+	return('\0');
+}
