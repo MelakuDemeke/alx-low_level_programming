54378ae674683704fc405530178c4401d054e6bc
MelakuDemeke <melaku.demeke789@gmail.com>
2023-01-02 11:23:55 +0300

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strsl - gets the length of a prefix substring
+ *
+ * @s: search key
+ * @accept: sub str
+ *
+ * Return: length of prifix substr
+ */
+
+unsigned int _strsl(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, k = 0, l;
+	int ischecked = 1;
+
+	while (s[i] != '\0' && ischecked)
+	{
+		j = 0;
+		l = k;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+				break;
+			}
+			else
+				j++;
+		}
+
+		if (l == k)
+			ischecked = 0;
+
+		i++;
+	}
+
+	return (k);
+}
