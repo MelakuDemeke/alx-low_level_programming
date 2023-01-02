4a958b07efd40726779124cfc6d9fd960280d9e2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-01-02 11:01:59 +0300

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _memset - fills memory with constant types
+*
+* @dest: destination buffer
+* @src: source to copy
+* @n: number of bytes of src
+*
+* Return: return copied memory area
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
