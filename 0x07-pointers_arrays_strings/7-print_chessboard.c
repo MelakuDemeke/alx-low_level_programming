25bd1992f33d3b5033a546ea0a8cf383b49c4de8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-01-02 12:01:54 +0300

    chase board code

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print a matrix
+ *
+ * @a: matrix
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+				_putchar('\n');
+		}
+	}
+}
