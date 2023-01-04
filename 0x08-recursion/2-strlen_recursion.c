873c406f7cd013f3d33f926fa3aa5993538dffaf
MelakuDemeke <melaku.demeke789@gmail.com>
2023-01-04 11:23:35 +0300

    str len with recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion -  returns the length of a string
+*
+* @s: string tobe checked
+*
+* Return: length of a string
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1))
+	}
+	else
+	{
+		return (0);
+	}
+}
