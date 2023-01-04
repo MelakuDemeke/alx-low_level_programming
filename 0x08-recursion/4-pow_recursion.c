047505176c4c583f8495fe2202966aba1dd44bd6
MelakuDemeke <melaku.demeke789@gmail.com>
2023-01-04 11:40:49 +0300

    betty fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,10 +11,10 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)	
+	if (y < 0)
 		return (-1);
 	else if (y == 0)
 		return (1);
 	else
-		return (x * _pow_recursion(x,y-1));
+		return (x * _pow_recursion(x, y-1));
 }
