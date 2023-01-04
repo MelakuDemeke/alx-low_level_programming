cf70811000cee4aa37af82241ac81f1a41470acb
MelakuDemeke <melaku.demeke789@gmail.com>
2023-01-04 14:24:52 +0300

    error found and removed

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int sqare_root(int n, int y)
+int sqare_root(int n, int y);
 int is_prime_number(int n);
 int wildcmp(char *s1, char *s2);
