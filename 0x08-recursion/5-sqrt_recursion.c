4df17b2aba2e823b50768a2fbf36f0c86735e562
MelakuDemeke <melaku.demeke789@gmail.com>
2023-01-04 12:04:59 +0300

    full fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,40 @@
 #include "main.h"
 
 /**
- * sqare_root - returns the power of number
+ * _sqrt_recursion_helper - a helper that does the sqare root
  *
- * @n: number
- * @y: counter
+ * @n : input number
+ * @min: minimum number to guess
+ * @max: maximum number to guess
  *
- * Return: square root
- */
- 
-int sqare_root(int n, int y)
-{
-	if (y * y == n)
-	{
-		return (y);
-	}
-	else if (y * y > n)
-	{
-		return (-1);
+ * Return: square root of n or -1
+*/
 
-	}
-	return (sqare_root(n, y + 1));
+int _sqrt_recursion_helper(int n, int min, int max)
+{
+	int guess, guess_squared;
+
+	guess = (min + max) / 2;
+	guess_squared = guess * guess;
+
+	if (guess_squared == n)
+		return (guess);
+	else if (min == max)
+		return (-1);
+	else if (guess_squared < n)
+		return (_sqrt_recursion_helper(n, guess + 1, max));
+	else
+		return (_sqrt_recursion_helper(n, min, guess - 1));
 }
 
-/**
-* _sqrt_recursion - eturns the natural square root of a number
-*
-* @n: number
-*
-* Return: natural square root
-*/
 int _sqrt_recursion(int n)
 {
-	return (sqare_root(n, 1));
+	if (n == 1)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (-1);
+	else
+		return (_sqrt_recursion_helper(n, 1, n));
 }
