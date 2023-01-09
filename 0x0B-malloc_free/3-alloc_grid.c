115a88b27ab7fba855935e3d6d447d1a29a9cae9
MelakuDemeke <melaku.demeke789@gmail.com>
2023-01-09 16:03:21 +0300

    minor bug && change to ||

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	int **matrix;
 	int i, j, l, *p;
 
-	if (width <= 0 && height <= 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 	matrix = (int **)malloc(height * sizeof(int *));
 	if (matrix == NULL)
