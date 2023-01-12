12b6240e7e4489705af3987a246f646d88018e91
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-12 11:42:17 +0300

    malloc checked added

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory
+ *
+ * @b: size of the memory
+ *
+ * Return: pointer;
+ **/
+
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+	     exit(98);
+	     return(NULL);
+	}
+	return (p);
+}
