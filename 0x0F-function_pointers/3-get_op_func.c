090315bb0a06818b1fae3719fb15f3ad3ec0e15b
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-18 14:50:21 +0300

    oprations function added

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_op_func - Find the equivalent function.
+ * @s: Operand.
+ * Return: Function address.
+ **/
+ 
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			break;
+		i++;
+	}
+return (ops[i].f);
+}
