c9bf918ca5172d3ef77e4197b106d24dbe327dc0
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-26 14:59:32 +0300

    add at head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_node - add node at head
+ * 
+ * @head: head address
+ * @str: address of new addition
+ * 
+ * Return: new address or NULL->if list_t=null
+ **/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	unsigned int len = 0;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	temp->str = strdup(str);
+	temp->len = len; 
+	temp->next = *head;
+	*head = temp;
+	return (*head);
+}
