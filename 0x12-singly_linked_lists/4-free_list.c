373304bee5f06b8568b29f114876607485bc2d16
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-26 15:30:05 +0300

    free starter

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ *
+ * @head: pointer to the list
+ *
+ * Return: no return
+ */
+
+void free_list(list_t *head)
+{
+
+}
