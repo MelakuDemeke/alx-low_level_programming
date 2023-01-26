2ee5e9c246777d2dfdbc5fa4f81aadd9d4dd7390
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-26 13:50:15 +0300

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - get number of elements
+ * 
+ * @h: pointer to the list
+ * 
+ * Return: 
+ */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *hold;
+	unsigned int count = 0;
+
+	hold = h;
+	while (hold)
+	{
+		count++;
+		hold = hold->next;
+	}
+	return (count);
+}
