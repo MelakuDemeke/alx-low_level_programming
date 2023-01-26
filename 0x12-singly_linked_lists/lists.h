b6c11171694e15918ce3a0d827b7c17aadcb432a
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-26 10:13:08 +0300

    data structure added

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,4 +2,19 @@
 #define LISTS_H
 #include <string.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 #endif /* list.h */
