ed22787fba7f7ff2bed92ebd1aeb57202218b99c
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-26 11:06:34 +0300

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - print elements on a singly linked list
+ *
+ * @h: pointer to list
+ *
+ * Return: int
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *hold;
+	unsigned int i = 0;
+
+	hold = h;
+	while (hold)
+	{
+		if (hold->str ==NULL)
+			printf("[%u] %s\n", 0, "(nil)");
+		else
+			printf("[%u] %s\n", hold->len, hold->str);
+		i++;
+		hold = hold->next;
+	}
+	return (i)
+}
