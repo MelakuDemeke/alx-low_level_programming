572e9b7a45a5aa15c52ac3ef9d35149789988787
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-26 15:16:35 +0300

    add node end starter

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_node_end - Add a new node at the end of list_t a list
+ *
+ * @head: first node address
+ * @str: address of string to be inserted
+ *
+ * Return: new node address
+ **/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	
+}
