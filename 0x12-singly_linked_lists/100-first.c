3a582731bbf3858b1221d6aa0b242acd159c622a
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-27 13:35:17 +0300

    minor change

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,7 @@
  * print - Prints before main is executed.
  **/
 
-void print(void) __attribute__ ((constructor));
-void print(void)
+void __attribute__ ((constructor)) premain();
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
