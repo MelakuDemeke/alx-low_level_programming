7dcd4db4059389fa8b59fa9b2201f09401caefa6
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-30 09:05:24 +0300

    data structure added

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,4 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 #endif
