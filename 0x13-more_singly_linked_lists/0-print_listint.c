c8a52db0a742953fd61993d02f4ed367bff6b5ef
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-31 10:04:30 +0300

    minor fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -17,10 +17,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h->next != NULL)
 	{
-		printf("%d\n", h->n);
-
-		h = h->next;
 		i++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	printf("%d\n", h->n);
 
