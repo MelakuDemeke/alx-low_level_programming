accfce09555984aeab80b09150bfd7f26e631130
Melaku Demeke <melaku.demeke789@gmail.com>
2023-03-16 15:46:21 +0300

    header changed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,10 +3,12 @@
 
 /**
  * print_dlistint - print all the elements of a dlistint_t list
+ *
  * @h: pointer to the start of the linked list
  *
  * Return: number of nodes
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
