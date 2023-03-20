6a7421460bbdc8581278f0373d22c53ea38b8aac
MelakuDemeke <melaku.demeke789@gmail.com>
2023-03-20 15:36:06 +0300

    change

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/MelakuDemeke/alx-low_level_programming/raw/main/0x18-dynamic_libraries/MelCrack.so
-export LD_PRELOAD=/tmp/iwin.so
+export LD_PRELOAD=/tmp/MelCrack.so
