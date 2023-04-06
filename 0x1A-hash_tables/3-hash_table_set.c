bca9c8aefc087130dbbe87720937342286bc4d1b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-06 11:58:18 +0300

    replace code added

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,6 +11,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index, i;
 	char *value_copy;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
@@ -19,4 +20,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
 }
