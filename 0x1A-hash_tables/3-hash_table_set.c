8c02e10a1d3aea33462efca25f8d991f40e69306
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-06 11:52:49 +0300

    doc added

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds or update an element to/in the hash table.
+ *
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key:  is the key. key can not be an empty string
+ * @value: The value associated with key, not duplicated
+ *
+ * Return: sucess -> 1 otherwise-> 0
+ */
