26a16f56289c98e1c27b222aedd5d9f4d93618f1
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-06 12:28:10 +0300

    return node value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
@@ -20,5 +21,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (index >= ht->size)
 		return (NULL);
 
-	
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
 }
