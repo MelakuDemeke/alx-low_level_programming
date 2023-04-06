f9687b56de89ddbedebad0cef0c0eb2a2741684e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-06 12:24:47 +0300

    doc updated

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get -  retrieves a value associated with a key
+ *
+ * @ht: hash table pointer
+ * @key: The key to get the value
+ *
+ * Return: value associated with element or NULL -> if key not found
+ */
