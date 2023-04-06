224d5c1fb1d2a5f1d4f0f90e7e76b0175976a7ec
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-06 11:10:53 +0300

    return null and return array added

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,4 +16,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (ht->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+
 }
