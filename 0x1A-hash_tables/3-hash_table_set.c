9506a0f657d50a90c017e56aafa8f8cef8c5eae7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-06 11:56:32 +0300

    check value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,6 +11,12 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	char *value_copy;
+
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
 }
