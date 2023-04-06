c210620d15436336ad11afac93082e81c03b1d29
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-06 11:24:48 +0300

    initial

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - give index to stor key and vlue in hash table
+ *
+ * @key: key to get index
+ * @size: hash table array size
+ *
+ * Return: index of the key
+ *
+ * Description: Uses the djb2 algorithm
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+}
