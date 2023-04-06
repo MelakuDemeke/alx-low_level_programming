eb657fd7d82284287a1c59675f2fa7e825fbf643
Melaku Demeke <melaku.demeke789@gmail.com>
2023-04-06 10:49:43 +0300

    define ht pointer and counter var

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs -> NULL.
+ *         Otherwise -> a new hash table pointer
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+}
