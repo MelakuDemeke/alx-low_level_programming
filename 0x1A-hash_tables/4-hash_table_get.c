f25dd4c35810294039a26b9d8c7bbdc6d7c1107e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-06 12:27:15 +0300

    chek if key is larger than size of ht

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,14 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	
 }
