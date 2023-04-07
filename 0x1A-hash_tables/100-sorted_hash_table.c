6d1124c3a7dff1b90cfd780b2fb4d964e59931e6
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-07 10:53:16 +0300

    doc added

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_create - Creates a sorted hash table
+ *
+ * @size: The size of new sorted hash table.
+ *
+ * Return: NULL -> if error, sorted ht -> otherwise
+ */
