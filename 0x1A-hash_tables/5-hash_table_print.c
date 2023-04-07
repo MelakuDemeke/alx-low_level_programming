d11a77d30952c823ad1485f569c783e9ae64d01d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-07 10:28:40 +0300

    add printer

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,7 +8,31 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
+	int i;
+	hash_node_t *node
+	unsigned char separet_flag = 0;
+
 	if (ht == NULL)
 		return;
-	
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (separet_flag == 1)
+				printf(", ");
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			separet_flag = 1;
+		}
+	}
+	printf("}\n");
 }
