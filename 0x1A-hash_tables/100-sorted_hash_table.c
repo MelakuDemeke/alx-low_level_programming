77a94c4c2dcdd16aebd7576465b01a0ef30c31ec
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-07 11:18:35 +0300

    put rev back

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,12 +1,5 @@
 #include "hash_tables.h"
 
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
-
 /**
  * shash_table_create - Creates a sorted hash table
  *
@@ -125,6 +118,30 @@ void shash_table_print(const shash_table_t *ht)
 	{
 		printf("'%s': '%s'", node->key, node->value);
 		node = node->snext;
+		if (node != NULL)
+			printf(", ");
+	}
+	printf("}\n");
+}
+
+/**
+ * shash_table_print_rev - print ht in revers order
+ *
+ * @ht: A pointer to the sorted hash table
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	shash_node_t *node;
+
+	if (ht == NULL)
+		return;
+
+	node = ht->stail;
+	printf("{");
+	while (node != NULL)
+	{
+		printf("'%s': '%s'", node->key, node->value);
+		node = node->sprev;
 		if (node != NULL)
 			printf(", ");
 	}
