490be37331fb5a78baa0b5e8ed96b3e33b0099ac
MelakuDemeke <melaku.demeke789@gmail.com>
2023-04-07 11:01:10 +0300

    ht create

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -10,5 +10,21 @@
 
 shash_table_t *shash_table_create(unsigned long int size)
 {
-	
+	shash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(shash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(shash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+	ht->shead = NULL;
+	ht->stail = NULL;
+
+	return (ht);
 }
