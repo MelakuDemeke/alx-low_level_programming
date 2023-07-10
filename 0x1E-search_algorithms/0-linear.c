94b36b4679406a7c3bd487b75dfa0b7f1818a30c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-10 11:29:55 +0300

    fix the formating

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,10 +21,12 @@ int linear_search(int *array, size_t size, int value)
 	{
 		if (array[i] == value)
 		{
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
-			return (i);
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
 		}
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		else
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 	}
 
 	return (-1);
