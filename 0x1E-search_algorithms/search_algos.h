8237549b2c78ab313e32e78b61d2be6babfb6884
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-11 12:12:12 +0300

    jump_search prototype added

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -40,5 +40,6 @@ typedef struct skiplist_s
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
