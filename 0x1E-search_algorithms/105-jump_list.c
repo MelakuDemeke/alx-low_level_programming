aa6aa9a993ac27fc921d93089621949c96f3f2e6
MelakuDemeke <melaku.demeke789@gmail.com>
2023-07-12 10:50:55 +0300

    fix the issue

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,46 +1,45 @@
 #include "search_algos.h"
 
 /**
- * jump_list - Searches for an algorithm in a sorted singly
- *             linked list of integers
- * @list: A pointer to the  head of the linked list to search.
+ * jump_list - Uses the jump search algorithm to find a value in a
+ *			 sorted singly linked list of integers.
+ * @list: A pointer to the head of the linked list.
  * @size: The number of nodes in the list.
  * @value: The value to search for.
  *
  * Return: If the value is not present or the head of the list is NULL, NULL.
- *			Otherwise, a pointer to the first node where the value is located.
+ *		 Otherwise, a pointer to the first node where the value is located.
  */
 
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t step = sqrt(size);
-	listint_t *node = list;
-	listint_t *jump = list;
+	size_t step, step_size;
+	listint_t *node, *jump;
 
 	if (list == NULL || size == 0)
 		return (NULL);
 
-	while (jump->index < size && jump->n < value)
+	step = 0;
+	step_size = sqrt(size);
+
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
 	{
 		node = jump;
-		for (size_t i = 0; i < step && jump->next; i++)
-			jump = jump->next;
+		for (step += step_size; jump->index < step; jump = jump->next)
+		{
+			if (jump->index + 1 == size)
+				break;
+		}
 		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
 	}
 
 	printf("Value found between indexes [%ld] and [%ld]\n",
-		node->index, jump->index);
+			node->index, jump->index);
 
-	while (node->index < jump->index && node->n < value)
-	{
+	for (; node->index < jump->index && node->n < value; node = node->next)
 		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-		node = node->next;
-	}
 
 	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
-	if (node->n == value)
-		return (node);
-	else
-		return (NULL);
+	return (node->n == value ? node : NULL);
 }
